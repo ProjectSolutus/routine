aedbbdb4e6e47b9b37c75efce16854c012ac9e5f
ARC <71553701+arcff5f00@users.noreply.github.com>
2024-07-23 18:33:00 +0530

    removed the task_model code

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,15 +1,1 @@
-import 'package:get/get.dart';
 
-class TaskModel {
-  final String id = "";
-  final String title = "";
-  final String description = "";
-  final bool isCompleted = false;
-
-  Task({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.icCompleted,
-  });
-}
