81613b3d165d6f827c9c1f53fa9a1cef966dae51
Kushagra <VolodarOlathana@tutanota.com>
2024-07-24 14:41:32 +0530

    feat(colorscheme): adds light and dark themes

---
+++ lib/themes/app_themes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppThemes{
+
+  static final ThemeData lightTheme = ThemeData(
+  colorScheme: ColorScheme(brightness: Brightness.light, primary: Color(0x2C2C2C), onPrimary: Color(0xF9F9F7), secondary: Color(0x2283E2), onSecondary: onSecondary, error: error, onError: onError, surface: surface, onSurface: onSurface)
+  )
+  static final ThemeData darkTheme = ThemeData(
+  )
+
+}
